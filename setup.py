f5c2af5d9d5b08d3093f7b345d3ef7fdb990230a
Andrey Vykhodtsev <anvykhod@microsoft.com>
2017-11-10 00:52:10 +0100

    pypi files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pypbi',
+  packages = ['pypbi'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'An Unofficial Python wrapper around PowerBI REST API',
+  author = 'Andrey Vykhodtsev',
+  author_email = 'vykhand@outlook.com',
+  url = 'https://github.com/vykhand/pypbi', # use the URL to the github repo
+  download_url = 'https://github.com/vykhand/pypbi/archive/0.0.1.tar.gz', # I'll explain this in a second
+  keywords = ['powerbi'], # arbitrary keywords
+  classifiers = [],
+)
