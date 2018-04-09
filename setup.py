7e71ff212d0c5b172ce01e85e1ba42fe0720a9d2
Andrey Vykhodtsev <anvykhod@microsoft.com>
2018-04-09 19:52:51 +0200

    version 0.0.3 files

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'pypbi',
   packages = ['pypbi'], # this must be the same as the name above
-  version = '0.0.2',
+  version = '0.0.3',
   description = 'An Unofficial Python wrapper around PowerBI REST API',
   author = 'Andrey Vykhodtsev',
   author_email = 'vykhand@outlook.com',
   url = 'https://github.com/vykhand/pypbi', # use the URL to the github repo
-  download_url = 'https://github.com/vykhand/pypbi/archive/0.0.2.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/vykhand/pypbi/archive/0.0.3.tar.gz', # I'll explain this in a second
   keywords = ['powerbi'], # arbitrary keywords
   install_requires=['pyyaml'],
   classifiers = [],
